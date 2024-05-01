ec1c1071a06c9a1b8e20b13668f8b778e69ebf80
ValNicolini <val_nicolini13@hotmail.com>
2024-04-30 22:55:59 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,17 +1,29 @@
-from Modelos.restaurante import Restaurante
-from Modelos.Cardapio.bebida import Bebida
-from Modelos.Cardapio.prato import Prato
+import requests
 
-restaurante_praca = Restaurante('praça', 'Gourmet')
-bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
-prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
-restaurante_praca.adicionar_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_no_cardapio(prato_paozinho)
-bebida_suco.aplicar_desconto()
-prato_paozinho.aplicar_desconto()
-def main():
-    restaurante_praca.exibir_cardapio()
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+response = requests.get(url)
+print(response)
 
+if response.status_code == 200:
+    dados_json = response.json()
+    print(dados_json)
+else:
+    print(f'O erro foi {response.status_code}')
 
-if __name__ == '__main__':
-    main()
+# from Modelos.restaurante import Restaurante
+# from Modelos.Cardapio.bebida import Bebida
+# from Modelos.Cardapio.prato import Prato
+#
+# restaurante_praca = Restaurante('praça', 'Gourmet')
+# bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+# prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
+# restaurante_praca.adicionar_no_cardapio(bebida_suco)
+# restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+# bebida_suco.aplicar_desconto()
+# prato_paozinho.aplicar_desconto()
+# def main():
+#     restaurante_praca.exibir_cardapio()
+#
+#
+# if __name__ == '__main__':
+#     main()
